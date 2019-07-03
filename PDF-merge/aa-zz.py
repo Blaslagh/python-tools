ef758b07ff3849a76e95112427f213fd4fd9a260
Blaslagh <iwanicki.a.m@gmail.com>
2019-07-03 21:04:15 +0200

    Some comments

--- PDF-merge/aa-zz.py
+++ PDF-merge/aa-zz.py
@@ -1,18 +1,27 @@
 import PyPDF2
 
+#Opening both files
+
 pdf1File = open('1.pdf', 'rb')
 pdf2File = open('2.pdf', 'rb')
 
 pdf1Reader = PyPDF2.PdfFileReader(pdf1File, strict=False)
 pdf2Reader = PyPDF2.PdfFileReader(pdf2File, strict=False)
+
+#Creating new object to store pages
+
 pdfWriter = PyPDF2.PdfFileWriter()
 
 for pageNum in range(pdf1Reader.numPages):
+    #adding pages from both files
     pdfWriter.addPage(pdf1Reader.getPage(pageNum))
     pdfWriter.addPage(pdf2Reader.getPage(pageNum))
 
+#Saving new pdf file
 pdfOutputFile = open('merged.pdf', 'wb')
 pdfWriter.write(pdfOutputFile)
+
+#Clearing the mess
 
 pdfOutputFile.close()
 pdf1File.close()
